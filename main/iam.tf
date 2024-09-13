f54c5cbb69fbe061189997b1bdb072c461f1292f
LuisMiranda10 <luiseduardocarneiro6@gmail.com>
2024-09-12 21:33:02 -0300

    creating iam role for lambda

--- main/iam.tf
+++ main/iam.tf
@@ -3,13 +3,31 @@ resource "aws_iam_policy" "lambda-policy-buckets" {
     policy = jsonencode({
         Version = "2012-10-27"
         Statement = [{
-            "Effect": "Allow"
-            "Action": "s3:GetObjects"
-            "Resource": "${aws_s3_bucket.source_bucket.arn}/*"
+            Effect: "Allow"
+            Action: "s3:GetObjects"
+            Resource: "${aws_s3_bucket.source_bucket.arn}/*"
     },  {
-            "Effect": "Allow",
-            "Action": "s3:PutObject",
-            "Resource": "${aws_s3_bucket.dest_bucket.arn}/*"
+            Effect: "Allow",
+            Action: "s3:PutObject",
+            Resource: "${aws_s3_bucket.dest_bucket.arn}/*"
         }]
     })
 }
+
+resource "aws_iam_role" "role_for_lambda" {
+    name = "LambdaRole"
+    assume_role_policy = jsonencode ({
+        Version = "2012-10-17"
+        Statement = [{
+            Action = "sts:AssumeRole"
+            Effect = "Allow"
+            Principal = {
+                Service = "lambda.amazonaws.com"
+            }
+        }]
+    })
+    tags = merge(local.common_tags, {
+        tag-key = "${namespaced_service_name}-role-lambda"
+    })
+}
+
