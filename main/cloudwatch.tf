7cf4c5f27439f44377b929aa0b9f5d18b39072a5
LuisMiranda10 <luiseduardocarneiro6@gmail.com>
2024-09-16 19:15:42 -0300

    creating cloudwatch log group

---
+++ main/cloudwatch.tf
@@ -0,0 +1,8 @@
+resource "aws_cloudwatch_log_group" "cloudwatch_logs_lambda" {
+    name = "/aws/lambda/${aws_lambda_function.lambda_function.function_name}"
+    retention_in_days = 30
+
+    tags = merge(local.common_tags, {
+        name = "${local.namespaced_service_name}-cloudwatch-log-lambda"
+      })
+}
