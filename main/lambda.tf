6ce81b50fff4be990a7ed31469cdf366b49c9ad5
LuisMiranda10 <luiseduardocarneiro6@gmail.com>
2024-09-16 15:28:36 -0300

    creating s3 bucket notification and lambda permission

--- main/lambda.tf
+++ main/lambda.tf
@@ -14,4 +14,27 @@ resource "aws_lambda_function" "lambda_function" {
   memory_size      = 256
   timeout          = 300
   handler          = "lambdaFunction.lambda_handler" # função lambda que vai ser usada
+
+  environment {
+    variables = {
+      dest_bucket = aws_s3_bucket.dest_bucket.bucket
+    }
+  }
+}
+
+resource "aws_lambda_permission" "thumbnail_s3_lambda_permission" {
+    statement_id = "AllowExecutionFromS3Bucket"
+    function_name = aws_lambda_function.lambda_function.function_name
+    principal = "s3.amazonaws.com"
+    action = "lambda:InvokeFunction"
+    source_arn = aws_s3_bucket.source_bucket.arn
+}
+
+resource "aws_s3_bucket_notification" "source_bucket_notification" {
+    bucket = aws_s3_bucket.source_bucket.id
+    lambda_function {
+      lambda_function_arn = aws_lambda_function.lambda_function.arn
+      events = ["s3:ObjectCreated:*"]
+    }
+    depends_on = [ aws_lambda_permission.thumbnail_s3_lambda_permission ]
 }
